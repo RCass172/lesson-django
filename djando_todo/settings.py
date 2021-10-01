c62c2aa4197bfd29c79ac6941780de005def8b85
RCass172 <ruthcassidy1221@gmail.com>
2021-10-01 16:26:02 +0000

    Remove secret key

--- djando_todo/settings.py
+++ djando_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'L>R-vd;F/SCcyt|38u#`l[}Eh:C~v4')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
